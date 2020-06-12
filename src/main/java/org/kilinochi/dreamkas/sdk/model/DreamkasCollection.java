260366ff2230dea4bf9b5e0e0979b584ff1865ea
arman.shamenov <arman.shamenov@corp.mail.ru>
2020-06-12 19:56:45 +0500

    product test

--- src/main/java/org/kilinochi/dreamkas/sdk/model/DreamkasCollection.java
+++ src/main/java/org/kilinochi/dreamkas/sdk/model/DreamkasCollection.java
@@ -3,6 +3,6 @@ package org.kilinochi.dreamkas.sdk.model;
 /**
  * @author arman.shamenov
  */
-public interface DreamkasCollection<T> extends DreamkasSerializable {
+public interface DreamkasCollection<T extends DreamkasSerializable> extends DreamkasSerializable {
     Class<T> getType();
 }
